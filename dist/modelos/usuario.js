a12518b953ff74e90256b9dffc23d168db7c9cb2
admin <admin@MacBook-Air-jijon.local>
2019-05-03 00:39:11 -0500

    anotaciones

--- dist/modelos/usuario.js
+++ dist/modelos/usuario.js
@@ -10,13 +10,17 @@ var rolesValidos = {
     message: '{VALUE} no es un rol permitido'
 };
 exports.usuarioSchema = new mongoose_1.Schema({
+    // squema (=new Schema el cual contiene role, nombre etc)
     role: { type: String, enum: rolesValidos, default: 'USER_ROL' },
     nombre: { type: String, required: [true, 'nombre necesario'] },
     apellido: { type: String, required: [true, 'apellido necesario'] },
-    email: { type: String, required: [true, 'correo necesario'] },
+    email: { type: String, unique: true, required: [true, 'correo necesario'] },
     password: { type: String, required: [true, 'contraseña necesaria'] }
-}, { collection: 'usuario' });
-exports.usuarioSchema.plugin(mongoose_unique_validator_1.default, { message: '{ PATH } debe ser unico' });
+}, { collection: 'usuario' } //en coleccion usuario guarda todos los registros ingresados
+);
+exports.usuarioSchema.plugin(mongoose_unique_validator_1.default, { message: '{ PATH } debe ser unico' }); //plugin es una extension que ayuda a unique a
+//mostrar el msg, PATH es el campo donde se ingresan los datos
 // (:)tipo (=)objeto (" ")nombre (' ')impresion
 //const usuario de (:) tipo modelo, llamado IUsuariomodel que es igual al objeto model llamado IUsuario model con nombre usuario 
-exports.Usuario = mongoose_1.model("Usuario", exports.usuarioSchema); // ya variable usuarioSchema
+exports.Usuario = mongoose_1.model("Usuario", exports.usuarioSchema); // y una variable usuarioSchema
+//< > indica que es el nombre del objeto que lo antecede
--- modelos/usuario.ts
+++ modelos/usuario.ts
@@ -2,29 +2,30 @@ import { Document, Schema, Model, model} from 'mongoose'; //mongoose es un mapea
 import uniqueValidator from 'mongoose-unique-validator';
 import { IUsuario } from '../interfaces/usuario';
 
-export interface IUsuarioModel extends IUsuario, Document{
-    fullName: string;
+export interface IUsuarioModel extends IUsuario, Document{  //se exporta la interfaz IUsuariomodel que es una extension o 
+    fullName: string;                                       //instancia IUsuario que va hacer un documento llamado fullName 
 }
-const rolesValidos = {
+const rolesValidos = {                    //es un arreglo de valores y mensaje
     values: ['ADMIN_ROLE', 'USER_ROLE'],  //no dejar espacio en los corchetes o marcara error
     message: '{VALUE} no es un rol permitido'
 }
 
-export var usuarioSchema: Schema = new Schema({
-
-    role: { type: String, enum: rolesValidos, default: 'USER_ROL' },
-    nombre: { type: String, required:[ true, 'nombre necesario']},
-    apellido: { type: String, required: [ true, 'apellido necesario']},
-    email: { type: String, required: [ true, 'correo necesario']},
+export var usuarioSchema: Schema = new Schema({    // variable usuarioSchema que es de tipo Schema y tiene una instancia del
+                                                   // squema (=new Schema el cual contiene role, nombre etc)
+    role: { type: String, enum: rolesValidos, default: 'USER_ROL' }, //enum enumero los roles validos, agrega por default el rol
+    nombre: { type: String, required:[ true, 'nombre necesario']}, //en require si no fuera obligatorio se le pone false y no true
+    apellido: { type: String, required: [ true, 'apellido necesario']}, 
+    email: { type: String, unique: true, required: [ true, 'correo necesario']},     //unique hace unico un valor 
     password: { type: String, required: [ true, 'contraseña necesaria']}
 },
 
-{ collection: 'usuario' }
+{ collection: 'usuario' } //en coleccion usuario guarda todos los registros ingresados
 
 );
 
-usuarioSchema.plugin(uniqueValidator, { message: '{ PATH } debe ser unico'});
-
+usuarioSchema.plugin(uniqueValidator, { message: '{ PATH } debe ser unico'}); //plugin es una extension que ayuda a unique a
+                                                                    //mostrar el msg, PATH es el campo donde se ingresan los datos
 // (:)tipo (=)objeto (" ")nombre (' ')impresion
 //const usuario de (:) tipo modelo, llamado IUsuariomodel que es igual al objeto model llamado IUsuario model con nombre usuario 
-export const Usuario: Model<IUsuarioModel> = model <IUsuarioModel> ("Usuario", usuarioSchema); // ya variable usuarioSchema
+export const Usuario: Model<IUsuarioModel> = model <IUsuarioModel> ("Usuario", usuarioSchema); // y una variable usuarioSchema
+                                                                     //< > indica que es el nombre del objeto que lo antecede
